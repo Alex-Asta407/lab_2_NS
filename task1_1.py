2b56b14e9c999a8399d5a4a03e33d711657ab9a4
Oleksandr Konstantinov <akonstantinov582@gmail.com>
2024-04-20 14:08:46 +0200

    first commit

---
+++ task1_1.py
@@ -0,0 +1,46 @@
+def legitimate_channel(input_word):
+    possible_errors = ["0000000", "0000001", "0000010",
+                       "0000100", "0001000", "0010000", "0100000", "1000000"]
+    output_words = []
+
+    for error in possible_errors:
+        output_word = ''.join(str(int(bit_a) ^ int(bit_b))
+                              for bit_a, bit_b in zip(input_word, error))
+        output_words.append(output_word)
+
+    return output_words
+
+
+def eavesdropper_channel(input_word):
+    possible_errors = ["0000000", "0000001", "0000010", "0000100", "0001000", "0010000", "0100000", "1000000",
+                       "1101000", "1110000"]
+    output_words = []
+
+    for error in possible_errors:
+        output_word = ''.join(str(int(bit_a) ^ int(bit_b))
+                              for bit_a, bit_b in zip(input_word, error))
+        output_words.append(output_word)
+
+    return output_words
+
+
+def is_valid_7bit_word(word):
+    if len(word) == 7 and all(bit in '01' for bit in word):
+        return True
+    else:
+        return False
+
+
+while True:
+    input_word = input("Enter a 7-bit word: ")
+    if is_valid_7bit_word(input_word):
+        break
+    else:
+        print("Invalid input. Please enter a 7-bit word consisting of 0s and 1s.")
+
+legitimate_output = legitimate_channel(input_word)
+print(f"Legitimate channel output for input {input_word}: {legitimate_output}")
+
+eavesdropper_output = eavesdropper_channel(input_word)
+print(
+    f"Eavesdropper channel output for input {input_word}: {eavesdropper_output}")
